91f97a31f7b2df54d233f71ce4be5652be7287e9
Aashish Neupane <np05cp4a220004@iic.edu.np>
2024-08-06 15:26:14 +0545

    signup-form

--- signup.php
+++ signup.php
@@ -6,17 +6,34 @@ include_once 'header.php';
 <section class="signup-form">
     <h2>Sign Up</h2>
     <form action="includes/signup.inc.php" method="post">
-    <input type="text" name="name" placeholder="Fullname...">
-    <input type="text" name="email" placeholder="Email...">
-    <input type="text" name="uid" placeholder="Username...">
-    <input type="password" name="pwd" placeholder="password...">
-    <input type="password" name="pwdrepeat" placeholder="Repeat password...">
+        <input type="text" name="name" placeholder="Fullname...">
+        <input type="text" name="email" placeholder="Email...">
+        <input type="text" name="uid" placeholder="Username...">
+        <input type="password" name="pwd" placeholder="password...">
+        <input type="password" name="pwdrepeat" placeholder="Repeat password...">
 
-    <button type="submit" name="submit">Sign Up</button>
+        <button type="submit" name="submit">Sign Up</button>
     </form>
-
+    <?php
+    if (isset($_GET["error"])) {
+        if ($_GET["error"] == "emptyinput") {
+            echo "<p>Fill in all fields !</p>";
+        } else if ($_GET["error"] == "invaliduid") {
+            echo "<p>Choose a proper username !</p>";
+        } else if ($_GET["error"] == "invalidemail") {
+            echo "<p>Choose a proper email !</p>";
+        } else if ($_GET["error"] == "passwordsdontmatch") {
+            echo "<p>Passwords doesn't match !</p>";
+        } else if ($_GET["error"] == "stmtfailed") {
+            echo "<p>Something went wrong, try again !</p>";
+        } else if ($_GET["error"] == "usernametaken") {
+            echo "<p>username already taken!</p>";
+        } else if ($_GET["error"] == "none") {
+            echo "<p>You have signed Up! </p>";
+        }
+    }
+    ?>
+</section>
 <?php
 include_once 'footer.php';
-
-
 ?>
