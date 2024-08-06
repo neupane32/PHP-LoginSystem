17e61e3359f7c77e9f61efa6f6d632e56fb57140
Aashish Neupane <np05cp4a220004@iic.edu.np>
2024-08-06 16:17:37 +0545

    header

--- header.php
+++ header.php
@@ -26,7 +26,7 @@ session_start();
                     <?php
                     if (isset($_SESSION["useruid"])) {
                         echo "<li><a href='profile.php'>Profile page</a></li>";
-                        echo "<li><a href='login.php'>Log out</a></li>";
+                        echo "<li><a href='includes/logout.inc.php'>Log out</a></li>";
                     } else {
                         echo "<li><a href='signup.php'>Sign up</a></li>";
                         echo "<li><a href='login.php'>Log in</a></li>";
